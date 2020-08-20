f7ebdcb988db586defa44ac127a60af6c8a59122
carlosdiazz <c.diazadriann@gmail.com>
2020-08-20 14:54:19 -0400

    Listo menu

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 <body>
 
 
-	<div class="wrapper">
+<div class="wrapper">
 
 		<header class="header">
 			<figure class="logo"><a href="index.php"><img src="img/logo.png" alt="Logo"></figure></a>
@@ -80,6 +80,7 @@
 
 			<div class="toggle"><img src="img/menu.svg"></div>
 		</header>
+
 
 
 
@@ -333,9 +334,7 @@
 	</div>
 
 
-	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV"	 crossorigin="anonymous"></script>
+	
 
 
 </body>
